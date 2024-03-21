6bae08d905891c1b58016fedbe33239c91f9f31b
Nabinbista12 <109153966+Nabinbista12@users.noreply.github.com>
2024-03-21 19:25:38 +0545

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 let typed = new Typed('#element', {
     strings: ['Web developement', 'App development', 'Python development', 'Frontend and Backend Development'],
     typeSpeed: 50,
-    loop: true
+    loop: false,
+    onComplete: function(self) {
+        self.reset()
+    }
 });
